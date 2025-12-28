311f1f043d85d7adcee722fbe800b379c019a77f
zackary-clark <zackary.a.clark@gmail.com>
2025-12-27 22:34:22 -0500

    wasm doesn't get more basic, but it does work

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,54 @@
 console.log("from typescript")
+
+function add() {
+    safelyCall(addJS);
+    safelyCall(addWASM);
+}
+
+function safelyCall(callback: Function) {
+    const timerLabel = callback?.name || "safelyCall";
+    let out: any;
+    console.time(timerLabel);
+    try {
+        out = callback();
+    } catch (e) {
+        console.error("error thrown by %s: ", timerLabel, e);
+    } finally {
+        console.timeEnd(timerLabel);
+    }
+    return out;
+}
+
+globalThis.add = add;
+
+function addWASM() {
+    globalThis.addWASM()
+}
+
+function addJS() {
+    const a = getNumberFromInput("a");
+    const b = getNumberFromInput("b");
+    console.log("a: ", a);
+    console.log("b: ", b);
+    const c = a + b;
+    outputToInput("out", c);
+    console.log("c: ", c);
+}
+
+function outputToInput(className: string, output: number) {
+    const outputElement = getElementByClassName("js-output")?.getElementsByClassName(className)[0];
+    outputElement.value = output;
+}
+
+function getNumberFromInput(className: string) {
+    const value = getElementByClassName(className)?.value;
+    return parseFloat(value);
+}
+
+function getElementByClassName(className: string) {
+    return getAddArticle()?.getElementsByClassName(className)[0];
+}
+
+function getAddArticle() {
+    return globalThis.document.getElementById("add")
+}
--- wasm/main.go
+++ wasm/main.go
@@ -1,7 +1,45 @@
+//go:build js && wasm
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"syscall/js"
+)
 
 func main() {
-	fmt.Println("from Go+WASM")
+	c := make(chan struct{}, 0)
+	fmt.Println("from Go+WASM, edited!")
+	js.Global().Set("addWASM", js.FuncOf(add))
+	<-c
+}
+
+func add(this js.Value, args []js.Value) any {
+	a, _ := getNumberFromInput("a")
+	b, _ := getNumberFromInput("b")
+	fmt.Println("a: ", a)
+	fmt.Println("b: ", b)
+	c := a + b
+	outputNumberToInput("out", c)
+	fmt.Println("c: ", c)
+	return ""
+}
+
+func getNumberFromInput(className string) (float64, error) {
+	value := getElementByClassName(className).Get("value").String()
+	return strconv.ParseFloat(value, 64)
+}
+
+func outputNumberToInput(className string, output float64) {
+	getElementByClassName("wasm-output").Call("getElementsByClassName", className).Index(0).Set("value", js.ValueOf(output))
+}
+
+func getElementByClassName(className string) js.Value {
+	elements := getAddArticle().Call("getElementsByClassName", className)
+	return elements.Index(0)
+}
+
+func getAddArticle() js.Value {
+	return js.Global().Get("document").Call("getElementById", "add")
 }
