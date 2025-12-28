aa6d08208c7444069d171c8febfa1ab07a32595e
zackary-clark <zackary.a.clark@gmail.com>
2025-12-28 15:04:19 -0500

    mult op
    mostly to have some code to refactor, a second example to design around

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,14 @@
+globalThis.multJS = multJS;
 globalThis.addJS = addJS;
+
+function multJS() {
+    const form: HTMLFormElement = globalThis.document.getElementById("mult-form");
+    const a = form.elements["a"].valueAsNumber;
+    const b = form.elements["b"].valueAsNumber;
+    const c = a * b;
+    const output = form.elements["js-out"];
+    output.value = c;
+}
 
 function addJS() {
     const form: HTMLFormElement = globalThis.document.getElementById("add-form");
--- wasm/main.go
+++ wasm/main.go
@@ -9,7 +9,18 @@ import (
 func main() {
 	c := make(chan struct{}, 0)
 	js.Global().Set("addWASM", js.FuncOf(add))
+	js.Global().Set("multWASM", js.FuncOf(mult))
 	<-c
+}
+
+func mult(this js.Value, args []js.Value) any {
+	form := js.Global().Get("document").Call("getElementById", "mult-form")
+	a := form.Get("elements").Get("a").Get("valueAsNumber").Float()
+	b := form.Get("elements").Get("b").Get("valueAsNumber").Float()
+	c := a * b
+	output := form.Get("elements").Get("wasm-out")
+	output.Set("value", js.ValueOf(c))
+	return ""
 }
 
 func add(this js.Value, args []js.Value) any {
