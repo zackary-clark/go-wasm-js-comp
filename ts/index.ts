c114170fa4dd8ece741ae91c17db2774583a1a77
zackary-clark <zackary.a.clark@gmail.com>
2025-12-28 12:53:59 -0500

    simplify and use form

--- ts/index.ts
+++ ts/index.ts
@@ -1,54 +1,10 @@
-console.log("from typescript")
-
-function add() {
-    safelyCall(addJS);
-    safelyCall(addWASM);
-}
-
-function safelyCall(callback: Function) {
-    const timerLabel = callback?.name || "safelyCall";
-    let out: any;
-    console.time(timerLabel);
-    try {
-        out = callback();
-    } catch (e) {
-        console.error("error thrown by %s: ", timerLabel, e);
-    } finally {
-        console.timeEnd(timerLabel);
-    }
-    return out;
-}
-
-globalThis.add = add;
-
-function addWASM() {
-    globalThis.addWASM()
-}
+globalThis.addJS = addJS;
 
 function addJS() {
-    const a = getNumberFromInput("a");
-    const b = getNumberFromInput("b");
-    console.log("a: ", a);
-    console.log("b: ", b);
+    const form: HTMLFormElement = globalThis.document.getElementById("add-form");
+    const a = form.elements["a"].valueAsNumber;
+    const b = form.elements["b"].valueAsNumber;
     const c = a + b;
-    outputToInput("out", c);
-    console.log("c: ", c);
-}
-
-function outputToInput(className: string, output: number) {
-    const outputElement = getElementByClassName("js-output")?.getElementsByClassName(className)[0];
-    outputElement.value = output;
-}
-
-function getNumberFromInput(className: string) {
-    const value = getElementByClassName(className)?.value;
-    return parseFloat(value);
-}
-
-function getElementByClassName(className: string) {
-    return getAddArticle()?.getElementsByClassName(className)[0];
-}
-
-function getAddArticle() {
-    return globalThis.document.getElementById("add")
+    const output = form.elements["js-out"];
+    output.value = c;
 }
--- wasm/main.go
+++ wasm/main.go
@@ -3,43 +3,21 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 	"syscall/js"
 )
 
 func main() {
 	c := make(chan struct{}, 0)
-	fmt.Println("from Go+WASM, edited!")
 	js.Global().Set("addWASM", js.FuncOf(add))
 	<-c
 }
 
 func add(this js.Value, args []js.Value) any {
-	a, _ := getNumberFromInput("a")
-	b, _ := getNumberFromInput("b")
-	fmt.Println("a: ", a)
-	fmt.Println("b: ", b)
+	form := js.Global().Get("document").Call("getElementById", "add-form")
+	a := form.Get("elements").Get("a").Get("valueAsNumber").Float()
+	b := form.Get("elements").Get("b").Get("valueAsNumber").Float()
 	c := a + b
-	outputNumberToInput("out", c)
-	fmt.Println("c: ", c)
+	output := form.Get("elements").Get("wasm-out")
+	output.Set("value", js.ValueOf(c))
 	return ""
-}
-
-func getNumberFromInput(className string) (float64, error) {
-	value := getElementByClassName(className).Get("value").String()
-	return strconv.ParseFloat(value, 64)
-}
-
-func outputNumberToInput(className string, output float64) {
-	getElementByClassName("wasm-output").Call("getElementsByClassName", className).Index(0).Set("value", js.ValueOf(output))
-}
-
-func getElementByClassName(className string) js.Value {
-	elements := getAddArticle().Call("getElementsByClassName", className)
-	return elements.Index(0)
-}
-
-func getAddArticle() js.Value {
-	return js.Global().Get("document").Call("getElementById", "add")
 }
